3585a382b942f99f7c7c338ab76b85fc398920bc
raicleysantana <raicleysantana1@gmail.com>
2019-11-03 22:17:41 -0400

    Criando rotas em frontend adicionado dependências no backend

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -11,7 +11,7 @@ module.exports = {
 
     async store(req,res){
         const { filename } = req.file;
-        const { company, techs, price } = req.body
+        const { company, techs, price } = req.body;
         const { user_id } = req.headers;
 
         const user = await User.findById(user_id);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-ktght.mongodb.net/semana09?retryWrites=true&w=majority',{
@@ -12,7 +12,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-ktght.mongodb.net/
 // req.query = acessar query params(para filtro)
 //req.params = Acessar route params (para edição,delete)
 //req.body = Acessar o corpo da requisição (para criação, edição  )
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
