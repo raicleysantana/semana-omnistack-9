da311bfe7cfd3979bfbc31e8adee86d3647e1148
raicleysantana <raicleysantana1@gmail.com>
2019-12-18 23:44:02 -0400

    Criado a tela de logn e spot

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,32 +1,64 @@
-import React from 'react';
-import { View, Image,KeyboardAvoidingView, Platform, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import api from '../services/api';
+
+import { AsyncStorage, View, Image, KeyboardAvoidingView, Platform, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 import logo from '../assets/logo.png';
 
-export default function Login(){
-    return ( <KeyboardAvoidingView enabled={Platform.OS == 'ios'} behavior="padding" style={style.container}>
+export default function Login({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user){
+                navigation.navigate('List');
+            }
+        });
+
+    },[]);
+
+    async function handleSubmit() {
+        const response = await api.post('/sessions', {
+            email
+        });
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
+
+    }
+
+    return (<KeyboardAvoidingView enabled={Platform.OS == 'ios'} behavior="padding" style={style.container}>
         <Image source={logo}></Image>
         <View style={style.form}>
             <Text style={style.label}>SEU E-MAIL *</Text>
-             <TextInput 
+            <TextInput
                 style={style.input}
                 placeholder="Seu e-mail"
                 placeholderTextColor="#999"
                 keyboardType="email-address"
                 autoCapitalize="none"
                 autoCorrect={false}
+                value={email}
+                onChangeText={setEmail}
             />
 
             <Text style={style.label}>TECNOLOGIAS *</Text>
-            <TextInput 
+            <TextInput
                 style={style.input}
                 placeholder="Tecnologias de interesses"
                 placeholderTextColor="#999"
                 autoCapitalize="words"
                 autoCorrect={false}
+                value={techs}
+                onChangeText={setTechs}
             />
 
-            <TouchableOpacity style={style.button}>
+            <TouchableOpacity onPress={handleSubmit} style={style.button}>
                 <Text style={style.buttonText}>Encontrar Spots</Text>
             </TouchableOpacity>
         </View>
@@ -34,41 +66,41 @@ export default function Login(){
 }
 
 const style = StyleSheet.create({
-    container : {
-        flex : 1,
-        justifyContent : 'center',
-        alignItems : 'center',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    form : {
-        alignSelf : 'stretch',
-        paddingHorizontal : 30,
-        marginTop:30,
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
     },
-    label : {
-        fontWeight : 'bold',
-        color : '#444',
-        marginBottom : 8,
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
     },
-    input : {
-        borderWidth : 1,
-        borderColor : '#ddd',
-        paddingHorizontal : 20,
-        fontSize : 16,
-        color : '#444',
-        height : 44,
-        marginBottom : 20,
-        borderRadius : 2,
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2,
     },
-    button : {
-        height : 42,
-        backgroundColor : '#f05a5b',
-        justifyContent : 'center',
-        alignItems : 'center',
-        borderRadius : 2, 
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
     },
-    buttonText : {
+    buttonText: {
         color: '#FFF',
-        fontWeight : 'bold',
-        fontSize : 16,
+        fontWeight: 'bold',
+        fontSize: 16,
     }
 });
